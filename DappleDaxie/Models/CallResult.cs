5a48fdc6e1bf148b01b6970d6bef48c8b6093728
Adam Lay <adam93.lay+git@gmail.com>
2015-05-26 18:09:38 +0100

    Fixing stuff

---
+++ DappleDaxie/Models/CallResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Script.Serialization;
+
+namespace DappleDaxie.Models
+{
+  public class CallResult
+  {
+    public bool Success { get; set; }
+    public object Result { get; set; }
+
+    public string ToJson()
+    {
+      return new JavaScriptSerializer().Serialize(this);
+    }
+  }
+}
---
+++ DappleDaxie/scripts/main.ts
@@ -0,0 +1,40 @@
+﻿$(document).ready(function ()
+{
+  
+
+  Data.Call("Images", { Foo: "bar" }, function (msg)
+  {
+    
+  });
+
+});
+
+class Data
+{
+  private static _callback: Function;
+  private static _lastCall: string;
+
+  public static Call(handler: string, data: Object, callback: (msg: any) => void): void
+  {
+    var input = {
+      Handler: handler,
+      Data: JSON.stringify(data)
+    };
+
+    Data._callback = callback;
+    Data._lastCall = handler;
+
+    console.log(Data._lastCall + " input: ", input);
+
+    $.post("handler.dap", input, Data.Callback);
+  }
+
+  public static Callback(json: string): void
+  {
+    var msg = JSON.parse(json);
+
+    console.log(Data._lastCall + " response: ", msg);
+
+    Data._callback(msg);
+  }
+}
